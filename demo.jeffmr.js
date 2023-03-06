a6e6f852077ead032ad59683d30ed05a37b87c22
Jefferson Miranda <contatejeff@gmail.com>
2023-03-05 22:22:04 -0300

    Create demo.jeffmr.js

---
+++ demo.jeffmr.js
@@ -0,0 +1,8 @@
+/* Coleta a mensagem através de "msgBlog" e envia para "notificacoes-barra" */
+var notificacoesBarra = document.getElementById("notificacoes-barra");
+var texto = document.getElementById("msgBlog").innerHTML;
+
+setInterval(function () {
+  texto = texto.substring(1) + texto.substring(0, 1);
+  notificacoesBarra.innerHTML = texto;
+}, 200);

b1ddb0c56c1dcdb4eedb5980a19753da6ce6daa6
Jefferson Miranda <contatejeff@gmail.com>
2023-03-07 01:04:49 -0300

    Criado o modelo básico com seo básica de otimização.

---
+++ demo.jeffmr/script.js
@@ -0,0 +1,12 @@
+/**********************************************/
+/************ Barra de notificação ************/
+/**********************************************/
+
+/* Coleta a mensagem através de "msgBlog" e envia para "notificacoes-barra" */
+var notificacoesBarra = document.getElementById("notificacoes-barra");
+var texto = document.getElementById("msgBlog").innerHTML;
+
+setInterval(function () {
+  texto = texto.substring(1) + texto.substring(0, 1);
+  notificacoesBarra.innerHTML = texto;
+}, 200);

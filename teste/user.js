2eafa1a6201ae553ee798470769cabd89eb21b09
Jefferson Miranda <contatejeff@gmail.com>
2023-03-09 00:50:39 -0300

    Update

--- teste/user.js
+++ teste/user.js
@@ -3,4 +3,14 @@ export default class User { // just add "default"
     constructor(name) {
       this.name = name;
     }
+    constructor(color){
+      this.color = color;
+    }
+    constructor(height){
+      this.height = height;
+    }
+    constructor(years){
+      this.years = years;
+    }
+
   }

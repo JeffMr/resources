a6407776d3567793d6553bd639294168f2853708
Jefferson Miranda <contatejeff@gmail.com>
2023-03-09 12:37:28 -0300

    removido demais construtores, só é permitido 1

--- teste/user.js
+++ teste/user.js
@@ -1,16 +1,7 @@
 // 📁 user.js
 export default class User { // just add "default"
-    constructor(name) {
-      this.name = name;
-    }
-    constructor(color){
-      this.color = color;
-    }
-    constructor(height){
-      this.height = height;
-    }
-    constructor(years){
-      this.years = years;
-    }
-
+  constructor(name) {
+    this.name = name;
   }
+
+}
